3a4a96449e81c8338463034ade8de0bff5a0c202
Wilson Page <wilsonpage@me.com>
2013-08-26 17:51:30 +0100

    Added first set of tests

---
+++ test/buster.js
@@ -0,0 +1,12 @@
+var config = module.exports;
+
+config["dom-batch"] = {
+  rootPath: '../',
+  environment: "browser",
+  sources: [
+    'lib/dom-batch.js'
+  ],
+  tests: [
+    'test/tests.js'
+  ]
+};
---
+++ test/setup.js
@@ -0,0 +1,2 @@
+
+var dom = new DomBatch();
