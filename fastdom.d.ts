b587c1ea46e0a6d94bcb9e17a460ca45c58305a5
Daniel Nagy <daniel_nagy@me.com>
2019-02-24 21:38:22 -0800

    better type definitions

--- fastdom.d.ts
+++ fastdom.d.ts
@@ -1,22 +1,12 @@
-// Type definitions for fastdom v1.0.4
-// Project: https://github.com/wilsonpage/fastdom
-// Definitions by: Martijn Welker <https://github.com/martijnwelker>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-declare namespace fastdom {
-    type CallbackFunction = () => void;
-
-    const reads: CallbackFunction[];
-    const writes: CallbackFunction[];
-    const scheduled: boolean;
-    const raf: (fn: CallbackFunction) => void;
-
-    function measure(fn: CallbackFunction, args?: any): CallbackFunction;
-
-    function mutate(fn: CallbackFunction, args?: any): CallbackFunction;
-
-    function clear(fn: CallbackFunction): boolean;
+declare class Fastdom {
+  clear<T extends () => void>(task: T): boolean;
+  extend<T extends object>(props: T): Omit<this, keyof T & keyof this> & T;
+  measure<T extends () => void>(task: T, context?: any): T;
+  mutate<T extends () => void>(task: T, context?: any): T;
 }
 
-export as namespace fastdom;
+declare const fastdom: Fastdom
+
 export = fastdom;
